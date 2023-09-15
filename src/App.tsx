5d5058fd25cb85185d0a17894f579d1096e751dc
sftblw <ccwpc@hanmail.net>
2023-09-16 00:52:25 +0900

    drag drop impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import { createSignal } from "solid-js";
 import "./App.css";
+import { For, createSignal, onCleanup, createEffect } from "solid-js";
 import RenamePatternInput from "./RenamePatternInput";
 
+import { appWindow } from "@tauri-apps/api/window";
+import { fs } from "@tauri-apps/api";
+
+export let [files, setFiles] = createSignal([] as string[]);
+export let [dropped, setDropped] = createSignal(false);
+export const [regex, setRegex] = createSignal("");
+export const [renamePattern, setRenamePattern] = createSignal("");
+
 function App() {
-  const [regex, setRegex] = createSignal("");
-  const [renamePattern, setRenamePattern] = createSignal("");
+  const renameFiles = () => {
+    let target_files = files();
+    target_files
+      .filter(it => it.match(regex()))
+      .forEach(it => fs.renameFile(it, it.replace(regex(), renamePattern())))
+      ;
+  }
+
+  createEffect(async () => {
+    let unlisten = await appWindow.onFileDropEvent(ev => {
+      let payload = ev.payload;
+      switch (payload.type) {
+        case 'hover': { setFiles(payload.paths); } break;
+        case 'drop': {
+          setFiles(payload.paths);
+          renameFiles()
+          setFiles([]);
+        } break;
+        case 'cancel': { setFiles([]); } break;
+      }
+    });
+    onCleanup(() => {
+      unlisten();
+    })
+  });
+
 
   return (
     <div class="container">
-      <RenamePatternInput setRegex={setRegex} setRenamePattern={setRenamePattern} />
-      <span>{regex()}</span>
+      <RenamePatternInput onRegexChanged={setRegex} onRenamePatternChanged={setRenamePattern} />
+      <span>{regex()} to {renamePattern()}</span>
+      <For each={files()} fallback={<div>No items</div>}>
+        {(file, index) => <div data-index={index()}>{file}</div>}
+      </For>
     </div>
   );
 }
--- src/RenamePatternInput.tsx
+++ src/RenamePatternInput.tsx
@@ -1,15 +1,15 @@
-import { createSignal, Setter } from "solid-js";
+import { Setter } from "solid-js";
 
 interface RenamePattern {
-    setRegex: Setter<string>,
-    setRenamePattern: Setter<string>
+    onRegexChanged: Setter<string>,
+    onRenamePatternChanged: Setter<string>
 }
 
 function RenamePatternInput(props: RenamePattern) {
     return (
         <div>
-            <input type="text" onInput={(ev) => props.setRegex(ev.target.value)} />
-            <input type="text" onInput={(ev) => props.setRenamePattern(ev.target.value)} />
+            <input type="text" onInput={(ev) => props.onRegexChanged(ev.target.value)} />
+            <input type="text" onInput={(ev) => props.onRenamePatternChanged(ev.target.value)} />
         </div>
     );
 }
