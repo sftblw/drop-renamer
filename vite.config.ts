7cc9a0f0dc59c5dca18d3a3bc8ed2acf160d45fa
sftblw <ccwpc@hanmail.net>
2023-10-02 00:59:29 +0900

    add plugin: vite-plugin-top-level-await

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import unocssPlugin from "unocss/vite"
 
+import topLevelAwait from "vite-plugin-top-level-await";
+
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [unocssPlugin(), solidPlugin()],
+  plugins: [
+    unocssPlugin(),
+    solidPlugin(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`
+    })
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
