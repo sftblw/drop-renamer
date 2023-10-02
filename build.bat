6c25559ba2c88a7feaad346532c656b1af046f42
sftblw <ccwpc@hanmail.net>
2023-10-02 17:30:21 +0900

    update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 call npm i || exit
-call npm run tauri build || exit
+call npm run tauri build -- --target x86_64-pc-windows-msvc || exit
+REM call npm run tauri build -- --target aarch64-pc-windows-msvc || exit
 wsl --exec ./build.sh
